cd85d06a01d9efebc2f33c06d3411a74707bbd38
Alexander Sirotin <Alexander.Sirotin@gmail.com>
2015-10-20 15:22:09 +0300

    Минимальный вариант для ExtJS 5.1.1

---
+++ app.js
@@ -0,0 +1,20 @@
+/*
+ * This file is generated and updated by Sencha Cmd. You can edit this file as
+ * needed for your application, but these edits will have to be merged by
+ * Sencha Cmd when upgrading.
+ */
+Ext.application({
+    name: 'ExtJS5Grid',
+
+    appFolder: 'app',    
+
+    extend: 'ExtJS5Grid.Application',
+    
+    autoCreateViewport: 'ExtJS5Grid.view.credit.CreditGrid'
+    
+    //-------------------------------------------------------------------------
+    // Most customizations should be made to ExtJS5Grid.Application. If you need to
+    // customize this file, doing so below this section reduces the likelihood
+    // of merge conflicts when upgrading to new versions of Sencha Cmd.
+    //-------------------------------------------------------------------------
+});
---
+++ app/view/credit/CreditController.js
@@ -0,0 +1,52 @@
+Ext.define('ExtJS5Grid.view.credit.CreditController', {
+    extend: 'Ext.app.ViewController',
+
+    requires: [
+        'Ext.window.MessageBox',
+        'ExtJS5Grid.view.credit.CreditForm'
+    ],
+
+    alias: 'controller.credit',
+
+    onPrint: function() {
+        window.open('http://www.yandex.ru/', '_blank');
+    },
+
+    onDelete: function(view, rowIndex, colIndex, item, e, record, row) {
+    var
+        title = 'Удаление',
+        msg = 'Вы действительно хотите удалить данную запись?';
+
+        Ext.Msg.confirm(title, msg, function(r) {
+            if ('yes' === r) {
+                record.drop();
+            }
+        });
+    },
+
+    onSave: function(btn, event) {
+    var 
+        win = btn.up('window'),
+        form = win.down('form');
+
+        form.submit({
+            url: 'index.html',
+            //method: 'GET',
+            success: function(form, action) {
+                Ext.Msg.alert('Выполнено', 'Сохранение выполнено успешно', function() {
+                     win.close();
+                });
+            },
+            failure: function(form, action) {
+                Ext.Msg.alert('Выполнено', 'Выполнено с ошибкой (нет ответа от сервера)', function() {
+                    win.close();
+                });
+            }
+        });
+    },
+
+    onClickToolbarButton: function (o, e) {
+        Ext.create('ExtJS5Grid.view.credit.CreditForm').show();
+    }
+
+});
